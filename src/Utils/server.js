9448a123dbedd08c9709cdf7c824b16ea72f26e3
hosain ali <hosainahmed534745@gmail.com>
2025-04-27 14:58:59 +0600

    update

--- src/Utils/server.js
+++ src/Utils/server.js
@@ -1,5 +1,4 @@
-// export const url = `${import.meta.env.VITE_URL}`;
-export const url = `http://13.49.200.198:5050`;
+export const url = `${import.meta.env.VITE_URL}`;
 export const imageUrl = (image) => {
   return image
     ? image?.startsWith(`http`)
